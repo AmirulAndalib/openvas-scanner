3d62b2c5376b4f1677b729d144e895a65c7800d7
Toni Peter <mail@tonipeter.de>
2025-11-10 14:23:32 +0100

    Add duplicate fn_arg lint.

--- rust/src/scannerctl/linter/cli.rs
+++ rust/src/scannerctl/linter/cli.rs
@@ -30,8 +30,8 @@ pub(super) fn get_files(path: &Path) -> Result<Vec<PathBuf>, CliError> {
     if path.is_file() {
         files.push(path.into())
     } else {
-        for e in walkdir::WalkDir::new(&path) {
-            let e = e.map_err(|e| std::io::Error::from(e))?;
+        for e in walkdir::WalkDir::new(path) {
+            let e = e.map_err(std::io::Error::from)?;
             if let Some("nasl") | Some("inc") = e.path().extension().and_then(|ext| ext.to_str()) {
                 files.push(e.path().to_owned())
             }
